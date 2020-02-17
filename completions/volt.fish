ae1a97c7d63a56c6b6aca75d92b40138cc3a28c1
konafx <22848261+konafx@users.noreply.github.com>
2020-02-17 23:09:16 +0900

    Refactoring v0.2?

--- completions/volt.fish
+++ completions/volt.fish
@@ -1,12 +1,16 @@
-function ___volt_get_profile
-    volt list -f '{{ .CurrentProfileName }}'
+function __fish_is_arg_n -a n
+    test $n -eq (count (string match -v -- '-*' (commandline -poc)))
+end
+
+function __volt_get_profile
+    volt list '{{ .CurrentProfileName }}'
 end
 
 function __volt_get_profiles
-    volt list -f '{{ range .Profiles }}{{ println .Name }}{{ end }}'
+    volt list '{{ range .Profiles }}{{ println .Name }}{{ end }}'
 end
 
-function __fish_volt_get_plugs -a profile target
+function __volt_get_plugs -a profile target
     if contains $profile (__volt_get_profiles)
         switch $target
             case all
@@ -22,45 +26,62 @@ function __fish_volt_get_plugs -a profile target
 end
 
 function __volt_all_plugs
-    volt list -f "{{ range .Repos }}{{ println .Path }}{{ end }}" | sed -E 's@^(www\.)?github\.com/@@' | sort -u
+    volt list "{{ range .Repos }}{{ println .Path }}{{ end }}" | sed -E 's@^(www\.)?github\.com/@@' | sort -u
 end
 
 function __volt_this_plug -a profile
-    volt list -f "{{ range .Profiles }}{{ if eq \"$profile\" .Name }}{{ range .ReposPath }}{{ println . }}{{ end }}{{ end }}{{ end }}" | sed -E 's@^(www\.)?github\.com/@@' | sort -u
+    volt list "{{ range .Profiles }}{{ if eq \"$profile\" .Name }}{{ range .ReposPath }}{{ println . }}{{ end }}{{ end }}{{ end }}" | sed -E 's@^(www\.)?github\.com/@@' | sort -u
 end
 
-complete -f -c volt -n "__fish_use_subcommand" -a "get" -d "Install or upgrade or add localy {repository} list as plugins"
-complete -f -c volt -n "__fish_seen_subcommand_from get" -a "(__volt_all_plugs)"
-complete -f -c volt -n "__fish_seen_subcommand_from get" -s l -d "use all plugins in current profile as targets"
-complete -f -c volt -n "__fish_seen_subcommand_from get" -s u -d "upgrade plugins"
+complete -c volt -f
+complete -c volt -n "__fish_use_subcommand" -a "get" -d "Install or upgrade or add repository list as plugins"
+complete -c volt -n "__fish_use_subcommand" -a "rm" -d "Uninstall one or more repository from every profile"
+complete -c volt -n "__fish_use_subcommand" -a "list" -d "Vim plugin information extractor"
+complete -c volt -n "__fish_use_subcommand" -a "enable" -d "Shortcut of: volt profile add -current"
+complete -c volt -n "__fish_use_subcommand" -a "disable" -d "Shortcut of: volt profile rm -current"
+complete -c volt -n "__fish_use_subcommand" -a "edit" -d "Open the plugconf file for editing"
+complete -c volt -n "__fish_use_subcommand" -a "profile" -d "profile"
+complete -c volt -n "__fish_use_subcommand" -a "build" -d "Build ~/.vim/pack/volt/ directory"
+complete -c volt -n "__fish_use_subcommand" -a "migrate" -d "Perform miscellaneous migration operations"
+complete -c volt -n "__fish_use_subcommand" -a "self-upgrade" -d "Upgrade to the latest volt command"
+complete -c volt -n "__fish_use_subcommand" -a "version" -d "Show volt command version"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "rm" -d "Uninstall one or more {repository} from every profile"
-complete -f -c volt -n "__fish_seen_subcommand_from rm" -a "(__volt_all_plugs)"
-complete -f -c volt -n "__fish_seen_subcommand_from rm" -s r -d "remove also repository directories of specified repositories"
-complete -f -c volt -n "__fish_seen_subcommand_from rm" -s p -d "remove also plugconf files of specified repositories"
+# get
+complete -c volt -n "__fish_seen_subcommand_from get" -s l -a "-u" -d "All plugins in current profile as targets"
+complete -c volt -n "__fish_seen_subcommand_from get" -s u -a "(__volt_all_plugs)" -d "Upgrade plugins"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "list" -d "Vim plugin information extractor"
-complete -f -c volt -n "__fish_seen_subcommand_from list" -s f -d "it renders by given template which can access the information of lock.json"
+# rm
+complete -c volt -n "__fish_seen_subcommand_from rm" -s r -d "Remove also repository directories"
+complete -c volt -n "__fish_seen_subcommand_from rm" -s p -d "Remove also plugconf files"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "enable" -d "This is shortcut of: volt profile add -current"
-complete -f -c volt -n "__fish_use_subcommand" -a "disable" -d "This is shortcut of: volt profile rm -current"
+# list
+complete -c volt -n "__fish_seen_subcommand_from list" -s f -d "It renders by given template which can access the information of lock.json"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "edit" -d "Open the plugconf file(s) of one or more repository for editing"
-complete -f -c volt -n "__fish_seen_subcommand_from edit" -a "(__volt_all_plugs)"
+# enable
+complete -c volt -n "__fish_seen_subcommand_from enable" -a "(__volt_get_plugins (__volt_get_profile) not)"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "profile" -d "profile"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "set" -d "Set profile"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "show" -d "Show profile info"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "list" -d "List all profiles"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "new" -d "Create new profile"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "destroy" -d "Delete profile"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "rename" -d "Rename profile {old} to {new}"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "add" -d "Add one or more repositories to profile"
-complete -f -c volt -n "__fish_seen_subcommand_from profile" -a "rm" -d "Remove one or more repositories to profile"
+# disable
+complete -c volt -n "__fish_seen_subcommand_from disable" -a "(__volt_get_plugins (__volt_get_profile) this)"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "build" -d "Build ~/.vim/pack/volt/ directory"
-complete -f -c volt -n "__fish_use_subcommand" -a "migrate" -d "Perform miscellaneous migration operations"
-complete -f -c volt -n "__fish_use_subcommand" -a "self-upgrade" -d "Upgrade to the latest volt command"
-complete -f -c volt -n "__fish_seen_subcommand_from self-upgrade" -s "check" -d "Only checks the newer version is available"
+# edit
+complete -c volt -n "__fish_seen_subcommand_from edit" -a "(__volt_all_plugs)"
+complete -c volt -n "__fish_seen_subcommand_from edit" -s e -l editor -a "vim"
 
-complete -f -c volt -n "__fish_use_subcommand" -a "version" -d "Show volt command version"
+# profile
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "set" -d "Set profile"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "show" -d "Show profile info"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "list" -d "List all profiles"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "new" -d "Create new profile"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "destroy" -d "Delete profile"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "rename" -d "Rename profile"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "add" -d "Add repository to profile"
+complete -c volt -n "__fish_seen_subcommand_from profile" -a "rm" -d "Remove repository to profile"
+
+# build
+
+# migrate
+
+# self-upgrade
+complete -c volt -n "__fish_seen_subcommand_from self-upgrade" -o "check" -d "Only checks the newer version is available"
+
+#version
